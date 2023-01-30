8c3144fe9ed5bc635629e74aee542d462595b7e5
0ttam <vtt2838@gmail.com>
2023-01-30 15:50:10 +0700

    #6 CRUD với React 2 - Hiển Thị Dữ Liệu Người Dùng Ra View (READ)

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,33 +1,71 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import { Container } from 'reactstrap';
+import './UserManage.scss';
+import { handleGetAllUsers } from '../../services/userService';
 class UserManage extends Component {
-
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: [],
+        };
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await handleGetAllUsers('ALL');
+        console.log(response);
+        this.setState({
+            arrUsers: response.users,
+        });
     }
-
 
     render() {
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className='users-container'>
+                <div className='title text-center'>Manage users</div>
+                <div className='users-table mt-4 mx-4'>
+                    <table>
+                        <tr>
+                            <th>Email</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Address</th>
+                            <th>Action</th>
+                        </tr>
+                        {arrUsers &&
+                            arrUsers.map((item, index) => {
+                                return (
+                                    <tr>
+                                        <td>{item.email}</td>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.address}</td>
+                                        <td>
+                                            <button className='pencil'>
+                                                <i className='fas fa-pencil-alt'></i>
+                                            </button>
+                                            <button className='trash'>
+                                                <i className='fas fa-trash'></i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                    </table>
+                </div>
+            </div>
         );
     }
-
 }
 
-const mapStateToProps = state => {
-    return {
-    };
+const mapStateToProps = (state) => {
+    return {};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+    return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
--- src/services/userService.js
+++ src/services/userService.js
@@ -4,5 +4,8 @@ import { stringify } from 'react-auth-wrapper/helpers';
 let handleLoginApi = (email, password) => {
     return axios.post('/api/login', stringify({ email, password }));
 };
+let handleGetAllUsers = (userId) => {
+    return axios.get(`/api/get-all-users?id=${userId}`);
+};
 
-export { handleLoginApi };
+export { handleLoginApi, handleGetAllUsers };
