67695bb212d4829899dd9fe50b9bbf42ee3cde6e
0ttam <vtt2838@gmail.com>
2023-02-20 16:13:47 +0700

    #18 Customize Header Admin Hiển Thị Đa Ngôn Ngữ (fix)

--- src/store/reducers/appReducer.js
+++ src/store/reducers/appReducer.js
@@ -32,7 +32,6 @@ const appReducer = (state = initialState, action) => {
                 },
             };
         case actionTypes.CHANGE_LANGUAGE:
-            console.log('state omn app reduced:',action)
             return {
                 ...state,
                 language: action.language,
