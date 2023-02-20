284a6789786129b127148e2108e379374040363f
0ttam <vtt2838@gmail.com>
2023-02-20 21:57:02 +0700

    #19 Phép Màu Khiến Redux Là Lựa Chọn Number 1 với React App

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,9 +1,9 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 
-import appReducer from "./appReducer";
-import adminReducer from "./adminReducer";
-import userReducer from "./userReducer";
+import appReducer from './appReducer';
+import adminReducer from './adminReducer';
+import userReducer from './userReducer';
 
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import storage from 'redux-persist/lib/storage';
@@ -17,19 +17,24 @@ const persistCommonConfig = {
 const adminPersistConfig = {
     ...persistCommonConfig,
     key: 'admin',
-    whitelist: ['isLoggedIn', 'adminInfo']
+    whitelist: ['isLoggedIn', 'adminInfo'],
 };
 
 const userPersistConfig = {
     ...persistCommonConfig,
     key: 'user',
-    whitelist: ['isLoggedIn', 'userInfo']
+};
+
+const appPersistConfig = {
+    ...persistCommonConfig,
+    key: 'app',
+    whitelist: ['language'],
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (history) => combineReducers({
-    router: connectRouter(history),
-    admin: persistReducer(adminPersistConfig, adminReducer),
-    user: persistReducer(adminPersistConfig, userReducer),
-    app: appReducer
-})
+export default (history) =>
+    combineReducers({
+        router: connectRouter(history),
+        user: persistReducer(adminPersistConfig, userReducer),
+        app: persistReducer(appPersistConfig, appReducer),
+    });
